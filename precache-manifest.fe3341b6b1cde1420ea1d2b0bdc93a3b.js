8d57305718f3ef480c07a4a3e82869b1797ea25c
Andrew Zacharias <andyzack@gmail.com>
2020-02-02 10:28:16 +0000

    Updates

---
+++ precache-manifest.fe3341b6b1cde1420ea1d2b0bdc93a3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ad4771cd050b89c94b567e9f869cea",
+    "url": "/my-edu/index.html"
+  },
+  {
+    "revision": "cd1ab6757182fee30ed6",
+    "url": "/my-edu/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/my-edu/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-edu/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd1ab6757182fee30ed6",
+    "url": "/my-edu/static/js/main.593e97af.chunk.js"
+  },
+  {
+    "revision": "9892bfa28176fb79fb9c",
+    "url": "/my-edu/static/js/runtime-main.2ead8d31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-edu/static/media/logo.5d5d9eef.svg"
+  }
+]);
